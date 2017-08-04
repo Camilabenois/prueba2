dc3904e7ab4fc9b23ed776523d86f75947058e94
Camilabenois <camilabenois@hotmail.com>
2017-08-03 20:27:50 -0400

    titulo

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
     				'<div class="twitter">'
                         +'<div class="eliminar glyphicon glyphicon-remove">'
                         +'</div>'
-                        +'<div class="twit">'
+                        +'<div class="row twit">'
     				        + texto
         				+'</div>'
                         +'<div class="icon">'
@@ -20,7 +20,33 @@
                 $("#mensaje").val('').focus();
             
     		});
+         $('#mensaje').on('keypress',function(e){
+            if (e.keyCode==13) return false
+            if (e.keyCode == 13 && e.shiftKey) return true   
+             
+         })  
+         $('#mensaje').on('keyup',function(e){
+            if (e.keyCode == 13) {
+            var texto= $("#mensaje").val();
+                event.preventDefault();
+                $(".right").prepend(
+                    '<div class="twitter">'
+                        +'<div class="eliminar glyphicon glyphicon-remove">'
+                        +'</div>'
+                        +'<div class="row twit">'
+                            + texto
+                        +'</div>'
+                        +'<div class="icon">'
+                            +'<div class="glyphicon glyphicon-heart">'
+                                + '<div class="contador pull-right">0'
+                                +'</div>'
+                            +'</div>'
+                        +'</div>'
+                    +'</div>')
+                $("#mensaje").val('').focus(); 
 
+            }
+         }) 
 
         $(".right").on("click", ".eliminar", function(){
             $(this).parent().fadeOut (800);
